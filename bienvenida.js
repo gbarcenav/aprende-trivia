a4e75817109ee2a414ed6c85e1b50f040c36266b
gbarcenav <51386502+gbarcenav@users.noreply.github.com>
2019-06-05 15:04:27 -0500

    Add files via upload

---
+++ bienvenida.js
@@ -0,0 +1,46 @@
+function recogeDatos(evento) {
+  evento.preventDefault();
+  var nombre = document.querySelector("#nombre").value;
+  var hola = document.querySelector("#hola");
+  var mensaje = "<p>¡Hola, " + nombre + "!</p>";
+
+  hola.innerHTML = mensaje;
+}
+
+var miForm = document.querySelector("#formulario");
+
+miForm.addEventListener("submit", recogeDatos);
+
+function escogeCategoria(evento) {
+  evento.preventDefault();
+  var categoriaColores = document.querySelector("#colores");
+  var categoriaAnimales = document.querySelector("#animales");
+}
+
+var botonColores = document.querySelector("#colores");
+var botonAnimales = document.querySelector("#animales");
+
+botonColores.addEventListener("submit", escogeCategoria);
+
+preguntaUnoC.addEventListener("submit", () => {
+  var respuesta = preguntaUnoC.value;
+  if (respuesta === "azul") {
+    count++;
+  }
+  console.log(count);
+});
+
+var colores = false;
+var animales = true;
+
+function iniciaColores() {
+  animales = true;
+  document.querySelector("#hola").classList.remove(visible);
+  iniciar();
+}
+
+function iniciaAnimales() {
+  animales = true;
+  document.querySelector("#hola").classList.remove(visible);
+  iniciar();
+}
