7f2c3ad00d47e999980caf24d1547d10dfb66aea
Gabriela Barcena <g.barcena.v@gmail.com>
2019-08-20 17:30:38 -0500

    Maqueté la primera sección de la web app (BIENVENIDA), agregué estilos para la sección. Empecé a maquetar la segunda sección (HOME) y empecé a agregar estilos.

--- bienvenida.js
+++ bienvenida.js
@@ -1,46 +1,46 @@
-function recogeDatos(evento) {
-  evento.preventDefault();
-  var nombre = document.querySelector("#nombre").value;
-  var hola = document.querySelector("#hola");
-  var mensaje = "<p>¡Hola, " + nombre + "!</p>";
+// function recogeDatos(evento) {
+//   evento.preventDefault();
+//   var nombre = document.querySelector("#nombre").value;
+//   var hola = document.querySelector("#hola");
+//   var mensaje = "<p>¡Hola, " + nombre + "!</p>";
 
-  hola.innerHTML = mensaje;
-}
+//   hola.innerHTML = mensaje;
+// }
 
-var miForm = document.querySelector("#formulario");
+// var miForm = document.querySelector("#formulario");
 
-miForm.addEventListener("submit", recogeDatos);
+// miForm.addEventListener("submit", recogeDatos);
 
-function escogeCategoria(evento) {
-  evento.preventDefault();
-  var categoriaColores = document.querySelector(".colores");
-  var categoriaAnimales = document.querySelector(".animales");
-}
+// function escogeCategoria(evento) {
+//   evento.preventDefault();
+//   var categoriaColores = document.querySelector(".colores");
+//   var categoriaAnimales = document.querySelector(".animales");
+// }
 
-var botonColores = document.querySelector("#colores");
-var botonAnimales = document.querySelector("#animales");
+// var botonColores = document.querySelector("#colores");
+// var botonAnimales = document.querySelector("#animales");
 
-botonColores.addEventListener("submit", escogeCategoria);
+// botonColores.addEventListener("submit", escogeCategoria);
 
-preguntaUnoC.addEventListener("submit", () => {
-  var respuesta = preguntaUnoC.value;
-  if (respuesta === "azul") {
-    count++;
-  }
-  console.log(count);
-});
+// preguntaUnoC.addEventListener("submit", () => {
+//   var respuesta = preguntaUnoC.value;
+//   if (respuesta === "azul") {
+//     count++;
+//   }
+//   console.log(count);
+// });
 
-var colores = false;
-var animales = true;
+// var colores = false;
+// var animales = true;
 
-function iniciaColores() {
-  animales = true;
-  document.querySelector("#hola").classList.remove(visible);
-  iniciar();
-}
+// function iniciaColores() {
+//   animales = true;
+//   document.querySelector("#hola").classList.remove(visible);
+//   iniciar();
+// }
 
-function iniciaAnimales() {
-  animales = true;
-  document.querySelector("#hola").classList.remove(visible);
-  iniciar();
-}
+// function iniciaAnimales() {
+//   animales = true;
+//   document.querySelector("#hola").classList.remove(visible);
+//   iniciar();
+// }
