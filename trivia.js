539a68fd671b4cb463db52c99bf99cd28fca07d3
Gabriela Barcena <g.barcena.v@gmail.com>
2019-08-22 23:39:42 -0500

    Agregué función para modales, realicé estilos para modales. Ya aparece el modal del SE ACABO EL TIEMPO y lo desaparece dando click en alguno de los botones. Arreglé los iconos de los botones de la sección HOME.

--- trivia.js
+++ trivia.js
@@ -32,10 +32,79 @@ let countdown = seconds => {
     // condicional para determinar que cuando el cronometro este en 0 va a parar y va a realizar una acción
     if (updatedTime === 0) {
       clearInterval(chronometer);
-      alert("¡Su tiempo ha terminado!");
+      printModal(modalTextBtns);
       document.getElementById("countdown").innerHTML = "0";
     } else {
       s--;
     }
   }, 1000);
 };
+
+// MODAL
+// Añadir un objeto de atributos a un elemento
+const addAttributes = (element, attrObj) => {
+  for (let attr in attrObj) {
+    if (attrObj.hasOwnProperty(attr)) element.setAttribute(attr, attrObj[attr]);
+  }
+};
+// Crear elementos con atributos e hijo
+const createCustomElement = (element, attributes, children) => {
+  let customElement = document.createElement(element);
+  if (children !== undefined)
+    children.forEach(el => {
+      if (el.nodeType) {
+        if (el.nodeType === 1 || el.nodeType === 11)
+          customElement.appendChild(el);
+      } else {
+        customElement.innerHTML += el;
+      }
+    });
+  addAttributes(customElement, attributes);
+  return customElement;
+};
+
+// Imprimir modal eliminar publicación
+const printModal = content => {
+  // Crear contenedor interno
+  const modalContent = createCustomElement(
+      "div",
+      {
+        id: "modal-content",
+        class: "modal-content"
+      },
+      [content]
+    ),
+    // Crear contenedor principal
+    modalContainer = createCustomElement(
+      "div",
+      {
+        id: "modal-container",
+        class: "modal-container"
+      },
+      [modalContent]
+    );
+
+  document.body.appendChild(modalContainer);
+
+  const removeModal = () => document.body.removeChild(modalContainer);
+
+  modalContainer.addEventListener("click", e => {
+    const btnAgain = document.getElementById("try-again");
+    const btnExit = document.getElementById("btn-exit");
+    if (e.target === btnAgain || e.target === btnExit) removeModal();
+  });
+};
+
+const modalTextBtns = ` <article class="modalTextBtns" id="modalTextTime">
+<h3>Se acabo el tiempo</h3>
+<figure>
+  <img src="./img/time-09.png" alt="sorprendido" class="img-feel"/>
+</figure>
+<button type="button" class="btn-white" >
+  <img src="./img/again-10.png" alt="otra vez" id="try-again"/>
+</button>
+<button type="button" class="btn-white"><img src="./img/exit-02.png" id="btn-exit"/></button>
+</article>`;
+// document.getElementById("").addEventListener("click", () => {
+//   printModal(modalTextBtns);
+// });
